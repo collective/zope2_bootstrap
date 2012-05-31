3c3cedbb7dd968aa71bca7681cfca75f32d4001f
Alex Clark <aclark@aclark.net>
2012-05-31 16:54:34 -0400

    Add buildout, zcml

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ from setuptools import setup
 
 VERSION = '0.0.1'
 
+
 setup(
     author='aclark4life',
     include_package_data=True,
+    install_requires=[
+        'collective.monkeypatcher',
+    ],
     name='zope2_bootstrap',
     packages=find_packages(),
     version=VERSION,
