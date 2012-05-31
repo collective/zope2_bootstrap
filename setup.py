395efee7f35912d95caebdd5b5486b94b2928c48
u38484 <u38484@938e6190-df3e-41d6-bd1b-22476e242e4a.(none)>
2012-05-31 20:42:38 +0000

    Package created by pythonpackages.com

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+VERSION = '0.0.1'
+
+setup(
+    author='aclark4life',
+    include_package_data=True,
+    name='zope2_bootstrap',
+    packages=find_packages(),
+    version=VERSION,
+)
