5479d9a6311b0502f806e0ee7ab7423fd19d6ffe
Alex Clark <aclark@aclark.net>
2012-05-31 23:42:06 -0400

    Add bootstrap

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
-
 VERSION = '0.0.1'
-
 
 setup(
     author='aclark4life',
+    description="This is what you think it is. Applies Twitter Bootstrap styles to Zope2",
+# XXX Any way to make this work with Zope2?
+#    entry_points = {
+#        'z3c.autoinclude.plugin': 'target = ???',
+#    },
     include_package_data=True,
     install_requires=[
         'Zope2',
--- zope2_bootstrap/patch.py
+++ zope2_bootstrap/patch.py
@@ -1,8 +1,14 @@
 from AccessControl import ClassSecurityInfo
+from AccessControl.class_init import InitializeClass
+from App.special_dtml import DTMLFile
+from App.Management import Navigation
+import os
+
 security = ClassSecurityInfo()
-
-
 security.declarePublic('manage_zmi_logout')
+
+
+# XXX Provide bogus class method to c.monkeypatcher
 def manage_zmi_logout(self, REQUEST, RESPONSE):
     """Logout current user"""
     p = getattr(REQUEST, '_logout_path', None)
@@ -16,8 +22,18 @@ def manage_zmi_logout(self, REQUEST, RESPONSE):
 <head><title>Logout</title></head>
 <body>
 <p>
-You have been logged out.
+You have been logged out. For real.
 </p>
 </body>
 </html>""")
     return
+
+
+# XXX Use callable to do the actual work
+def apply_patch(scope, original, replacement):
+    here = os.path.dirname(__file__)
+    manage_page_style = os.path.join(here, 'bootstrap', 'css', 'bootstrap.css')
+    dtmlfile = DTMLFile(manage_page_style, globals())
+    security.declarePublic('manage_page_style.css')
+    setattr(Navigation, 'manage_page_style.css', dtmlfile)
+    InitializeClass(Navigation) 
