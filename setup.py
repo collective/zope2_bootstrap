11e19cfa7ec2c7711c279a7bbd0d0c7e1363e1f4
Alex Clark <aclark@aclark.net>
2012-06-12 08:28:22 -0400

    Wording

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ VERSION = '0.0.7'
 setup(
     author='Alex Clark',
     author_email='aclark@aclark.net',
-    description="This does what you think it does: applies Twitter Bootstrap styles to Zope2",
+    description="This does what you think: applies Twitter Bootstrap styles to Zope2",
+
 # XXX Any way to make this work with Zope2?
 #    entry_points = {
 #        'z3c.autoinclude.plugin': 'target = ???',
 #    },
+
     include_package_data=True,
     install_requires=[
         'Zope2',
