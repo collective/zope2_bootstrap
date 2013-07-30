6c1d0fd0ca6b0ecb83d946d3d5c1cec4dd610ac8
Alex Clark <aclark@aclark.net>
2013-07-30 19:49:27 -0400

    Fix error(s)

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setup(
     install_requires=[
         'collective.monkeypatcher',
     ],
-    long_description=(open('README.rst').read() + '\n'
+    long_description=(
+        open('README.rst').read() + '\n' +
         open(os.path.join('docs', 'HISTORY.txt')).read()),
     name='zope2_bootstrap',
     packages=find_packages(),
