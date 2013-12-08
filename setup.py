1ff1f07c7f09da7c6c4b704a2ac8053c7b9643c5
Alex Clark <aclark@aclark.net>
2013-12-08 15:37:44 -0500

    Add meta

--- setup.py
+++ setup.py
@@ -9,16 +9,23 @@ VERSION = '0.0.8'
 setup(
     author='Alex Clark',
     author_email='aclark@aclark.net',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
     description="ZMI Bootstrapped",
+    keywords='Bootstrap Plone Zope',
     include_package_data=True,
     install_requires=[
         'collective.monkeypatcher',
     ],
+    license='GPL',
     long_description=(
         open('README.rst').read() + '\n' +
         open('CHANGES.rst').read()),
     name='zope2_bootstrap',
     packages=find_packages(),
+    test_suite='tests.TestCase',
     url='https://github.com/aclark4life/zope2_bootstrap',
     version=VERSION,
+    zip_safe=False,
 )
