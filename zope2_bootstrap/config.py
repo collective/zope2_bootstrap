e8300cc4b546d165fdd3584d91c49c6b49ec627e
Alex Clark <aclark@aclark.net>
2015-07-19 07:28:22 -0400

    Clean up

--- zope2_bootstrap/config.py
+++ zope2_bootstrap/config.py
@@ -1,33 +1,32 @@
-LOGO_PLONE = """\
+################################################################################
+LOGO_PLONE = """
 <div style="margin: 22px 0 22px 0">
-    <a href="<dtml-var "REQUEST.SERVER_URL" html_quote>"><img
-        src="<dtml-var "REQUEST.SERVER_URL"
-        html_quote>/++resource++plone-logo.png"></a>
+    <a href="<dtml-var "REQUEST.SERVER_URL" html_quote>"><img src="<dtml-var
+        "REQUEST.SERVER_URL" html_quote>/++resource++plone-logo.png"></a>
 </div>
 """
 
-LOGO_ZOPE = """\
-    <a href="<dtml-var "REQUEST.SERVER_URL" html_quote>"><img
-        src="<dtml-var "REQUEST.SERVER_URL"
-        html_quote>/++resource++VlogoWhite250.gif"></a>
+LOGO_ZOPE = """
+    <a href="<dtml-var "REQUEST.SERVER_URL" html_quote>"><img src="<dtml-var
+        "REQUEST.SERVER_URL" html_quote>/++resource++VlogoWhite250.gif"></a>
 """
 
-LOGOUT = """<html>
-<head><title>Logout</title></head>
-<body>
-    <p>
-        You have been logged out. For real.
-    </p>
-</body>
+LOGOUT = """
+<html>
+    <head>
+        <title>Logout</title>
+    </head>
+    <body>
+        <p>You have been logged out.</p>
+    </body>
 </html>
 """
 
 ZMI_WARNING = """
 <div class="alert alert-warning alert-dismissable">
     <button type="button" class="close" data-dismiss="alert">&times;</button>
-    <strong>Warning:</strong> the <span>Zope Management Interface (ZMI)
-    provides direct access to Zope database objects (ZODB).
-    Do not attempt to copy, cut, paste, add, edit or remove content or modify
-    settings unless you know exactly what you are doing!
+    <strong>Warning:</strong> this management interface provides direct access
+    to database objects. Do not cut, copy, paste, add, edit or delete content,
+    or modify settings, unless you know what you are doing.
 </div>
 """
