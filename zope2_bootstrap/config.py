934ea5831165f3e798b4d0706af983ca88b6aeaf
Alex Clark <aclark@aclark.net>
2015-07-19 11:27:08 -0400

    Clean up

--- zope2_bootstrap/config.py
+++ zope2_bootstrap/config.py
@@ -1,15 +1,3 @@
-LOGO_PLONE = """
-<div style="margin: 22px 0 22px 0">
-    <a href="<dtml-var "REQUEST.SERVER_URL" html_quote>"><img src="<dtml-var
-        "REQUEST.SERVER_URL" html_quote>/++resource++plone-logo.png"></a>
-</div>
-"""
-
-LOGO_ZOPE = """
-    <a href="<dtml-var "REQUEST.SERVER_URL" html_quote>"><img src="<dtml-var
-        "REQUEST.SERVER_URL" html_quote>/++resource++VlogoWhite250.gif"></a>
-"""
-
 LOGOUT = """
 <html>
     <head>
@@ -21,11 +9,23 @@ LOGOUT = """
 </html>
 """
 
-ZMI_WARNING = """
+PLONE_LOGO = """
+<div style="margin: 22px 0 22px 0">
+    <a href="<dtml-var "REQUEST.SERVER_URL" html_quote>"><img src="<dtml-var
+        "REQUEST.SERVER_URL" html_quote>/++resource++plone-logo.png"></a>
+</div>
+"""
+
+WARNING = """
 <div class="alert alert-warning alert-dismissable">
     <button type="button" class="close" data-dismiss="alert">&times;</button>
     <strong>Warning:</strong> this management interface provides direct access
-    to database objects. Do not cut, copy, paste, add, edit or delete content,
-    or modify settings, unless you know what you are doing.
+    to database objects. Please do not cut, copy, paste, add, edit or delete
+    content or modify settings unless you know what you're doing.
 </div>
+"""
+
+ZOPE_LOGO = """
+    <a href="<dtml-var "REQUEST.SERVER_URL" html_quote>"><img src="<dtml-var
+        "REQUEST.SERVER_URL" html_quote>/++resource++VlogoWhite250.gif"></a>
 """
--- zope2_bootstrap/patch.py
+++ zope2_bootstrap/patch.py
@@ -1,25 +1,30 @@
-from .config import LOGO_PLONE
-from .config import LOGO_ZOPE
 from .config import LOGOUT
-from .config import ZMI_WARNING
-from App.special_dtml import DTMLFile
+from .config import PLONE_LOGO
+from .config import WARNING
+from .config import ZOPE_LOGO
+
 from App.Management import Navigation
+from App.special_dtml import DTMLFile
 from OFS.ObjectManager import ObjectManager
 from OFS.interfaces import IApplication
 from ZPublisher.BaseRequest import DefaultPublishTraverse
+
 from zope.component import adapts
 from zope.component import queryMultiAdapter
 from zope.interface import Interface
 from zope.publisher.interfaces import IRequest
+
 import Products
 import os
 
+# Based on
+#    https://github.com/plone/Products.CMFPlone/blob/master/Products/\
+#    CMFPlone/browser/admin.py#L34
 
-# Based on https://github.com/plone/Products.CMFPlone/blob/master/Products/\
-# CMFPlone/browser/admin.py#L34
 class AppTraverser(DefaultPublishTraverse):
     """
     """
+
     adapts(IApplication, IRequest)
 
     def publishTraverse(self, request, name):
@@ -33,21 +38,64 @@ class AppTraverser(DefaultPublishTraverse):
         return DefaultPublishTraverse.publishTraverse(self, request, name)
 
 
-def cook(code, main, target):
+def apply_patch(scope, original, replacement):
     """
-    Cook resources
     """
+    here = os.path.dirname(__file__)
+
+    # Add Bootstrap
+
+    manage_main = os.path.join(here, 'manage_main')  # Our custom manage_main
+    manage_page_header = os.path.join(here, 'manage_page_header')
+    manage_page_footer = os.path.join(here, 'manage_page_footer')
+    dtmlfile = DTMLFile(manage_page_header, globals())
+    setattr(Navigation, 'manage_page_header', dtmlfile)
+    dtmlfile = DTMLFile(manage_page_footer, globals())
+    setattr(Navigation, 'manage_page_footer', dtmlfile)
+
+    # Add table class
+
+    ObjectManager.manage_main = DTMLFile(manage_main, globals())
+
+    # Add warning
+
+    target = '<table width="100%" cellspacing="0" cellpadding="2"'
+    target += ' border="0">'
+    html = WARNING
+    main = ObjectManager.manage_tabs
+    cook(html, main, target)
+
+    # Add logo
+
+    target = '<table cellpadding="0" cellspacing="0" width="100%"'
+    target += ' border="0">'
+
+    if has_plone():
+        html = PLONE_LOGO
+    else:
+        html = ZOPE_LOGO
+
+    main = ObjectManager.manage_tabs
+    cook(html, main, target)
+
+
+def cook(html, main, target):
+    """
+    """
+
     orig = main.read()
     pos = orig.find(target)
-    new = orig[:pos] + code + orig[pos:]
+    new = orig[:pos] + html + orig[pos:]
     main.edited_source = new
     main._v_cooked = main.cook()
 
+# c.monkeypatcher requires a function or method so we give it this one, even
+# though we don't need to patch it.
 
-# XXX c.monkeypatcher requires a function or method so we give it one, though
-# we don't need to patch this particular method at all.
 def manage_zmi_logout(self, REQUEST, RESPONSE):
-    """Logout current user"""
+    """
+    """
+
     p = getattr(REQUEST, '_logout_path', None)
     if p is not None:
         return apply(self.restrictedTraverse(p))
@@ -60,40 +108,7 @@ def manage_zmi_logout(self, REQUEST, RESPONSE):
 
 
 def has_plone():
+    """
+    """
+
     return hasattr(Products, 'CMFPlone')
-
-
-def apply_patch(scope, original, replacement):
-    """
-    Patch DTML
-    """
-    here = os.path.dirname(__file__)
-
-    # Use Twitter Bootstrap CSS/JavaScript
-    manage_main = os.path.join(here, 'manage_main')  # Our custom manage_main
-    manage_page_header = os.path.join(here, 'manage_page_header')
-    manage_page_footer = os.path.join(here, 'manage_page_footer')
-    dtmlfile = DTMLFile(manage_page_header, globals())
-    setattr(Navigation, 'manage_page_header', dtmlfile)
-    dtmlfile = DTMLFile(manage_page_footer, globals())
-    setattr(Navigation, 'manage_page_footer', dtmlfile)
-
-    # Use class="table" on folder contents
-    ObjectManager.manage_main = DTMLFile(manage_main, globals())
-
-    # Add ZMI warning
-    target = '<table width="100%" cellspacing="0" cellpadding="2"'
-    target += ' border="0">'
-    code = ZMI_WARNING
-    main = ObjectManager.manage_tabs
-    cook(code, main, target)
-
-    # Add Zope or Plone logo
-    target = '<table cellpadding="0" cellspacing="0" width="100%"'
-    target += ' border="0">'
-    if has_plone():
-        code = LOGO_PLONE
-    else:
-        code = LOGO_ZOPE
-    main = ObjectManager.manage_tabs
-    cook(code, main, target)
